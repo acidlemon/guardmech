b79a4bc69aa941bfafde1139957ec2036ca44cbf
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2019-09-10 00:23:29 +0900

    set log flags

--- cmd/guardmech/main.go
+++ cmd/guardmech/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/acidlemon/guardmech"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
 
 	m := guardmech.New()
 	m.Run()
