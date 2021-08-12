ee818c3263f034e28b7d2fe8bd7745067271c0d2
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2021-08-12 13:41:55 +0900

    ファイル追加忘れ

---
+++ frontend/src/components/bootstrap/BAlert.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="'alert ' + alertVariant" role="alert">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+
+type Props = {
+   variant: string
+}
+
+export default defineComponent({
+  name: 'BAlert',
+  props: {
+    variant: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props: Props) {
+    const alertVariant = computed(() => {
+      if (!props.variant) {
+        return ''
+      }
+      return `alert-${props.variant}`
+    })
+
+    return {
+      alertVariant,
+    }
+  },
+})
+</script>
